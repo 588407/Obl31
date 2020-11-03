9352a92de6f52ca233cc5c07c8b2dde3c6e6697d
588407 <54099032+588407@users.noreply.github.com>
2020-11-03 14:46:34 +0100

    Add files via upload

---
+++ DiceController.js
@@ -0,0 +1,11 @@
+function rollDice(){
+	var die1 = document.getElementById("die1");
+	var status = document.getElementById("status");
+	var d1 = Math.floor(Math.random() * 6) + 1;
+	die1.innerHTML = d1;
+	status.innerHTML = "Du trillet "+d1+".";
+	
+	
+	
+	
+}
